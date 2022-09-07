c99da4d8f0d468209549aed40bdb9525e52e31a4
unknown <reynaldo.bjsz@gmail.com>
2022-09-07 10:23:41 -0600

    CV added

--- src/components/buttons/CvButton.js
+++ src/components/buttons/CvButton.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import resume from "../../../static/resume.pdf"
 
 const CvButton = () => {
     return (
@@ -6,9 +7,8 @@ const CvButton = () => {
             className='text-xl px-16 py-2 2xl:py-4 bg-red-800 rounded-2xl font-bold
             text-white border-4 border-red-800 hover:bg-white hover:border-gray-200
             hover:text-red-700 shadow-lg'
-            href='https://www.facebook.com/reynaldo.bejarano/'
-            target="_blank"
-            rel="noreferrer"
+            href={resume}
+            download="reynaldoBejaranoCV.pdf"
         >
             Download CV
         </a>
